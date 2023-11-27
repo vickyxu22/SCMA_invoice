87b851383044b1a91eba4f22cc532a5fc4a0c465
Vicky <113194676+vickyxu22@users.noreply.github.com>
2023-11-27 18:05:07 -0500

    add publisher and publication date

--- SCMA_invoice.py
+++ SCMA_invoice.py
@@ -1,5 +1,6 @@
 from docx import Document
 import os
+import re
 import pandas as pd
 
 # Directory path to search within the Desktop folder
@@ -7,7 +8,7 @@ desktop_path = os.path.expanduser("~/Desktop")  # Path to the Desktop folder
 folder_name = "scma"  # Name of the folder within Desktop
 folder_path = os.path.join(desktop_path, folder_name)  # Path to the 'scma' folder
 
-# List to store extracted information
+# Lists to store extracted information
 all_data = []
 
 try:
@@ -25,6 +26,9 @@ try:
             doc = Document(file_path)
             author = None
             publication = None
+            formats = None
+            publisher = None
+            publication_date = None
 
             # Extract information from each file
             for paragraph in doc.paragraphs:
@@ -39,13 +43,35 @@ try:
                             author = parts[0].strip()
                             publication = parts[1].strip()
 
-                            # Append extracted data to the list as a tuple (author, publication)
-                            all_data.append((author, publication))
-                        else:
-                            print(f"Error: Unable to extract Author and Title of Publication from '{file_name}'.")
+                if "Formats:" in paragraph.text:
+                    # Extract the word after "Formats:"
+                    formats_segment = paragraph.text.split("Formats:")
+                    if len(formats_segment) > 1:
+                        formats = formats_segment[1].strip()
+
+                if "Publisher:" in paragraph.text:
+                    # Extract the word after "Publisher:"
+                    publisher_segment = paragraph.text.split("Publisher:")
+                    if len(publisher_segment) > 1:
+                        publisher = publisher_segment[1].strip()
+
+                if "Publication date:" in paragraph.text:
+                    # Use regular expression to find the year pattern (four consecutive digits)
+                    date_segment = paragraph.text.split("Publication date:")
+                    if len(date_segment) > 1:
+                        match = re.search(r'\b\d{4}\b', date_segment[1])
+                        if match:
+                            publication_date = match.group()
+
+            # Append extracted data to the list as a tuple (author, publication, formats, publisher, publication_date)
+            all_data.append((author, publication, formats, publisher, publication_date))
 
         # Create a DataFrame from all extracted data
-        data = {'Author': [entry[0] for entry in all_data], 'Publication': [entry[1] for entry in all_data]}
+        data = {'Author': [entry[0] for entry in all_data],
+                'Publication': [entry[1] for entry in all_data],
+                'Formats': [entry[2] for entry in all_data],
+                'Publisher': [entry[3] for entry in all_data],
+                'Publication Date': [entry[4] for entry in all_data]}
         df = pd.DataFrame(data)
 
         # Export DataFrame to Excel
@@ -58,4 +84,3 @@ try:
 
 except Exception as e:
     print(f"An error occurred: {e}")
-
